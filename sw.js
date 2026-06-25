9aed4bf80da29503ccbe1118fd87e2b383df1907
foodiespoint <prime.aksh@gmail.com>
2026-06-25 13:18:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // FOODIES POINT - SERVICE WORKER (V59)
 // ==========================================================================
 
-const CACHE_NAME = 'foodies-cache-v85';
+const CACHE_NAME = 'foodies-cache-v86';
 const ASSETS = ['', 'index.html', 'app.js', 'manifest.json', 'icon.png'];
 
 self.addEventListener('install', (event) => {
